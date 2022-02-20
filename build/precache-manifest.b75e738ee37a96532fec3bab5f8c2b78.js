4559c146b6a91b55a090cece7a3dba36cbb1408d
kishan030 <43406019+kishan030@users.noreply.github.com>
2022-02-21 01:28:18 +0530

    Adding files

---
+++ build/precache-manifest.b75e738ee37a96532fec3bab5f8c2b78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "419c70d1870389bb782ecb4bea64ff00",
+    "url": "/SGP/index.html"
+  },
+  {
+    "revision": "0eaf428aa16943d5ed5d",
+    "url": "/SGP/static/css/2.c7a63f68.chunk.css"
+  },
+  {
+    "revision": "a96b518780ffef83837e",
+    "url": "/SGP/static/css/main.fabc7ab0.chunk.css"
+  },
+  {
+    "revision": "0eaf428aa16943d5ed5d",
+    "url": "/SGP/static/js/2.dc559fb9.chunk.js"
+  },
+  {
+    "revision": "a96b518780ffef83837e",
+    "url": "/SGP/static/js/main.3b87c84a.chunk.js"
+  },
+  {
+    "revision": "2180d097bbbb6da7e3b7",
+    "url": "/SGP/static/js/runtime~main.ab66ce29.js"
+  }
+]);
